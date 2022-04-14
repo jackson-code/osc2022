f918efc9fce1de89c0a3a95a7f364878573236cb
jackson-code <jacksonlp3333@gmail.com>
2022-04-14 13:41:33 +0800

    lab4 all finish

---
+++ lab4/kernel8 (b1)/include/el1.h
@@ -0,0 +1,15 @@
+#ifndef __EL1_H__
+#define __EL1_H__
+
+
+#include "cpio.h"
+#include "uart.h"
+#include "my_string.h"
+
+//#define NEW_ADDR	((char *)0x20000)// user program addr
+
+
+void el_user_start();
+
+
+#endif
---
+++ lab4/kernel8 (b1)/src/el1.c
@@ -0,0 +1,46 @@
+#include "el1.h"
+
+void el_user_start()
+{
+	char *file_addr = cpio_get_addr("user.img");
+	uart_puts("user address : ");
+	uart_put_hex((unsigned long)file_addr);
+
+
+/*
+	int app_size = find_app_size("user.img");
+	uart_puts("user prog size : ");
+	uart_put_int(app_size);
+
+	char *target = NEW_ADDR;
+	while(app_size--){
+		*target=*file_addr;
+		target++;
+		file_addr++;
+	}
+	*/
+	uart_puts("loading user prog...\n");
+
+	if (!str_cmp("no file", file_addr)) {
+		uart_puts("ERROR: no file");
+	}
+	else {
+		uart_puts("switch EL1 to EL0...");
+		
+		assert_receive_interrupt();
+		assert_transmit_interrupt();
+		
+		//asm volatile("mov	x0, 0x3c0		\n");		// using sp_el0, interrupt disable
+		asm volatile("mov	x0, 0x340		\n");		// using sp_el0, interrupt enable
+		asm volatile("msr	spsr_el1, x0	\n");
+		
+		// set stack pointer
+		asm volatile("ldr	x1, =0x160000	\n");
+		asm volatile("msr	sp_el0, x1		\n");
+		
+		// set return address, return to the user.S
+		asm volatile("msr	elr_el1, %0		\n"::"r"(file_addr));
+		asm volatile("eret					\n");
+	}
+}
+
